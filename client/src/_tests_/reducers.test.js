50ff9d28890d3d1c2ebe59e5824495cf65f38185
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-05-07 18:11:46 -0400

    import new cart actions from utils in test file

--- client/src/_tests_/reducers.test.js
+++ client/src/_tests_/reducers.test.js
@@ -4,8 +4,14 @@ import { reducer } from '../utils/reducers';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-} from '../utils/actions';
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+  } from '../utils/actions';
   
 // create a sample of what our global state will look like
 const initialState = {
